9350b0c202fca99007d7eace7f0ffa8669a56c44
SchaafLaura <schaaf.laura92@gmail.com>
2024-04-08 16:54:07 +0200

    pushie :3

--- Program.cs
+++ Program.cs
@@ -1,19 +1,37 @@
-﻿// See https://projecteuler.net/problem=54
+﻿using System;
+// See https://projecteuler.net/problem=54
 Console.WriteLine("Haiiii :3333");
 
+class Hand
+{
+    Card[] cards;
+}
 
+class Card
+{
+    int value;
+    Suite suite;
+    
+    public Card(string str)
+    {
 
+    }
+}
+
+enum Suite
+{
+    HEARTS,
+    CLUBS,
+    SPADES,
+    DIAMONDS
+}
 
 /*
-each card is represented by two chars
-
-
+each card is represented by two chars:
 V,S
 
 V = card Value
 S = card Suite
-
-
 */
 
 
