da8e02eefcc3acf43d487b697c58dffbe2d53fb8
SchaafLaura <schaaf.laura92@gmail.com>
2024-04-08 16:38:51 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,3 +2,30 @@
 Console.WriteLine("Haiiii :3333");
 
 
+
+
+/*
+each card is represented by two chars
+
+
+V,S
+
+V = card Value
+S = card Suite
+
+
+*/
+
+
+/*
+card Values:
+2 = 2 
+3 = 3
+...
+9 = 9
+T = 10
+J = 11
+Q = 12
+K = 13
+A = 14
+*/
